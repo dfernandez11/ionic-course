6992cf09689d81e638b557872c57698ee4b79712
dfernandez11 <dfernandez1110@egmail.com>
2017-01-04 17:58:25 -0300

    ceating a persisting user data

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -79,6 +79,8 @@ Controller for the favorites page
   // get the list of our favorites from the user service
   $scope.favorites = User.favorites;
 
+  $scope.username = User.username;
+
   $scope.removeSong = function(song, index) {
     User.removeSongFromFavorites(song, index);
   }	
@@ -91,7 +93,7 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope, User, Recommendations) {
+.controller('TabsCtrl', function($scope, User, Recommendations, $window) {
   // expose the number of new favorites to the scope
   $scope.favCount = User.favoriteCount;
 
@@ -108,6 +110,31 @@ Controller for our tab bar
   $scope.enteringFavorites = function() {
     User.newFavorites = 0;
     Recommendations.haltAudio();
+  }
+
+    $scope.logout = function() {
+    User.destroySession();
+
+    // instead of using $state.go, we're going to redirect.
+    // reason: we need to ensure views aren't cached.
+    $window.location.href = 'index.html';
+  }
+
+})
+
+.controller('SplashCtrl', function($scope, $state, User) {
+
+  // attempt to signup/login via User.auth
+  $scope.submitForm = function(username, signingUp) {
+    User.auth(username, signingUp).then(function(){
+      // session is now set, so lets redirect to discover page
+      $state.go('tab.discover');
+
+    }, function() {
+      // error handling here
+      alert('Hmm... try another username.');
+
+    });
   }
 
 });
